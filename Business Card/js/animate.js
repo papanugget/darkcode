820ef16e2cc8912b696a4741d04e014e9f486a65
papanugget <evilgenius04@gmail.com>
2019-07-19 14:57:26 -0400

    animated card flip

--- Business Card/js/animate.js
+++ Business Card/js/animate.js
@@ -1,3 +1,11 @@
 // select business card
 // add event listener for click
 // toggle active class
+const card = document.querySelector('.business-card');
+// console.log(card);
+card.addEventListener('click', flipCard);
+
+function flipCard() {
+    // console.log('card clicked');
+    card.classList.toggle('business-card-active');
+}
